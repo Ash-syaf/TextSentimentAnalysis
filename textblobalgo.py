76f6e051e429bce44b9615a21c5b9ec509f86682
Ash-syaf <83003783+Ash-syaf@users.noreply.github.com>
2024-01-08 23:59:00 +0800

    Update textblobalgo.py

--- textblobalgo.py
+++ textblobalgo.py
@@ -1,6 +1,6 @@
 from textblob import TextBlob
 import streamlit as st
-import streamlit.components.vl as components
+import streamlit.components.v1 as components
 from PIP import Image
 
 #Create user defined function
