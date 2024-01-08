0023650fb18a7d502ed5bad468883bc4203d9eba
Ash-syaf <83003783+Ash-syaf@users.noreply.github.com>
2024-01-09 00:06:11 +0800

    update v1

--- textblobalgo.py
+++ textblobalgo.py
@@ -11,7 +11,7 @@ def displayPage():
   st.text(' ')
   if st.button('Predict'):
     if (userText != ''):
-      st.components.vl.html("""<h3 style="color:#0284c7; font-family:Source Sans Pro,sans-serif; font-size:28px; margin-bottom:8px; margin-top:55px;">RESULT</h3>""", height=150)
+      st.components.v1.html("""<h3 style="color:#0284c7; font-family:Source Sans Pro,sans-serif; font-size:28px; margin-bottom:8px; margin-top:55px;">RESULT</h3>""", height=150)
       getSentiment(userText)
       
 #Write user define function getSentiment
