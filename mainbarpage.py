84b7c1f7fd0f326da55eebc634a855fd4e145ac1
Ash-syaf <83003783+Ash-syaf@users.noreply.github.com>
2024-01-09 10:39:49 +0800

    Update mainbarpage.py

--- mainbarpage.py
+++ mainbarpage.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import sidebarpage
 import textblobalgo
+import texttoemotion
 page = sidebarpage.show()
 if (page == 'TextBlob Analysis'):
   textblobalgo.displayPage()
+elif (page == 'Text2Emotion Analysis'):
+  texttoemotion.displayPage()
