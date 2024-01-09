e64810a6857050d82f66d659438d1eeb7e60d400
Ash-syaf <83003783+Ash-syaf@users.noreply.github.com>
2024-01-09 10:48:10 +0800

    Update mainbarpage.py

--- mainbarpage.py
+++ mainbarpage.py
@@ -2,8 +2,11 @@ import streamlit as st
 import sidebarpage
 import textblobalgo
 import texttoemotion
+import vaderalgo
 page = sidebarpage.show()
 if (page == 'TextBlob Analysis'):
   textblobalgo.displayPage()
 elif (page == 'Text2Emotion Analysis'):
   texttoemotion.displayPage()
+elif page == 'VADER Sentiment Analysis':
+  vaderalgo.displayPage()
